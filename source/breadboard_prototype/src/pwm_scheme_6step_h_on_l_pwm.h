8e396ae9231049e58979070cd6625cff0efda153
Piotr Esden-Tempski <piotr@esden.net>
2009-05-03 20:33:10 +0200

    Implemented H_on_L_pwm scheme.

--- source/breadboard_prototype/src/pwm_scheme_6step_h_on_l_pwm.h
+++ source/breadboard_prototype/src/pwm_scheme_6step_h_on_l_pwm.h
@@ -16,13 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef __PWM_SCHEMES_H
-#define __PWM_SCHEMES_H
+#ifndef __PWM_SCHEME_6STEP_H_ON_L_PWM
+#define __PWM_SCHEME_6STEP_H_ON_L_PWM
 
-#include "pwm_scheme_6step_h_pwm_l_on.h"
-#include "pwm_scheme_6step_h_on_l_pwm.h"
-#include "pwm_scheme_6step_on_pwm.h"
-//#include "pwm_scheme_6step_pwm_on.h"      /* TODO: implement */
-#include "pwm_scheme_12step_pwm_on_pwm.h"
+void pwm_scheme_6step_h_on_l_pwm(void);
 
-#endif /* __PWM_SCHEMES_H */
+#endif /* __PWM_SCHEME_6STEP_H_ON_L_PWM */

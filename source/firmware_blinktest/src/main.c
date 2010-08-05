ab6d32df2204000da0686aa9a7930b911dd02b14
Piotr Esden-Tempski <piotr@esden.net>
2010-08-04 23:46:37 -0700

    Updated blinktest to match new pinout of v0.3 hardware.

--- source/firmware_blinktest/src/main.c
+++ source/firmware_blinktest/src/main.c
@@ -39,29 +39,70 @@ void my_delay(unsigned long delay)
 	}
 }
 
+void led_on(int id)
+{
+	switch(id) {
+	case 0:
+		LED_RED_ON();
+		break;
+	case 1:
+		LED_GREEN_ON();
+		break;
+	case 2:
+		LED_BLUE_ON();
+		break;
+	case 3:
+		LED_ORANGE_ON();
+		break;
+	}
+}
+
+void led_off(int id)
+{
+	switch(id) {
+	case 0:
+		LED_RED_OFF();
+		break;
+	case 1:
+		LED_GREEN_OFF();
+		break;
+	case 2:
+		LED_BLUE_OFF();
+		break;
+	case 3:
+		LED_ORANGE_OFF();
+		break;
+	}
+}
+
 int main(void)
 {
-	int i, j;
+	int i, j, led_id;
 
 	system_init();
 	led_init();
 
+	led_id = 0;
+
 	while (1) {
 		for (j = 0; j < 20; j++) {
 			for (i = 0; i < 125; i++) {
-				LED_RED_ON();
+				led_on(led_id);
 				my_delay(50 * j);
-				LED_RED_OFF();
+				led_off(led_id);
 				my_delay(1200 - 50 * j);
 			}
 		}
 		for (j = 0; j < 20; j++) {
 			for (i = 0; i < 125; i++) {
-				LED_RED_OFF();
+				led_off(led_id);
 				my_delay(200 + 50 * j);
-				LED_RED_ON();
+				led_on(led_id);
 				my_delay(1000 - 50 * j);
 			}
 		}
+		led_off(led_id);
+		led_id++;
+		led_id %= 4;
 	}
 }

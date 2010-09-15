89699dcf708cd5977d4818747889e3f79284f76a
Tobias Fuchs <twh.fuchs@gmail.com>
2010-09-14 17:32:18 -0700

    Cleanup in yamlgen after extracing OBLDC-specific parts

--- source/utils/yamlgen/include/deprec/error_handling.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef ERROR_HANDLING_H__
-#define ERROR_HANDLING_H__
-
-#include <yaml.h>
-
-void on_parser_error(yaml_parser_t * parser); 
-void on_emitter_error(yaml_emitter_t * emitter); 
-
-#endif

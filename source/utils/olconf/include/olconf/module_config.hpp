8a6d58e4c63960ff141e40f90d9ca5f0be3117db
Tobias Fuchs <twh.fuchs@gmail.com>
2010-09-16 17:13:29 -0700

    ModuleConfigBuilder now expands sub-configs in olconf

--- source/utils/olconf/include/olconf/module_config.hpp
+++ source/utils/olconf/include/olconf/module_config.hpp
@@ -1,6 +1,10 @@
 #ifndef MODULE_CONFIG_HPP__
 #define MODULE_CONFIG_HPP__
 
+#include <olconf/register_config_builder.hpp>
+#include <olconf/flag_config_builder.hpp>
+#include <olconf/define_config_builder.hpp>
+
 #include <yamlgen/config_node.hpp>
 
 
@@ -15,12 +19,42 @@ private:
 	::std::string m_name; 
 	ConfigNode m_config_node; 
 
+private: 
+
+	::std::vector<RegisterConfigBuilder> m_registers;
+	::std::vector<FlagConfigBuilder> m_flags; 
+	::std::vector<DefineConfigBuilder> m_defines; 
+
+public: 
+
+	::std::vector<RegisterConfigBuilder> & registers() { 
+		return m_registers;
+	}
+	::std::vector<FlagConfigBuilder> & flags() { 
+		return m_flags;
+	}
+	::std::vector<DefineConfigBuilder> & defines() { 
+		return m_defines;
+	}
+
 public: 
 
 	ModuleConfig(::std::string const & name, 
 							 ConfigNode const & config_node) 
 	: m_name(name), m_config_node(config_node) 
 	{ } 
+
+public: 
+
+	void add(RegisterConfigBuilder const & builder) { 
+		m_registers.push_back(builder); 
+	}
+	void add(DefineConfigBuilder const & builder) { 
+		m_defines.push_back(builder); 
+	}
+	void add(FlagConfigBuilder const & builder) { 
+		m_flags.push_back(builder); 
+	}
 
 public: 
 
--- source/utils/olconf/src/module_config_builder.cpp
+++ source/utils/olconf/src/module_config_builder.cpp
@@ -2,6 +2,9 @@
 #include <olconf/module_config_builder.hpp>
 #include <olconf/module_config.hpp>
 #include <olconf/abstract_module_config_runner.hpp>
+#include <olconf/module_config_builder.hpp>
+#include <olconf/register_config_builder.hpp>
+#include <olconf/define_config_builder.hpp>
 
 #include <yamlgen/config_node.hpp>
 #include <yamlgen/logging.hpp>
@@ -34,7 +37,40 @@ ModuleConfigBuilder::parse_partial(ConfigNode const & config_node)
 		::std::string module_name     = (*it_modules).first; 
 		ConfigNode module_config_node = (*it_modules).second; 
 		ModuleConfig module_config(module_name, module_config_node);
+
+		ConfigNode::const_iterator modes_it; 
+		ConfigNode::const_iterator modes_end = module_config_node.end(); 
+		for(modes_it = module_config_node.begin(); modes_it != modes_end; ++modes_it) { 
+			::std::string mode_name = (*modes_it).first;
+			ConfigNode mode_config  = (*modes_it).second; 
+
+/* TODO: Refactor to abstract factory pattern: 
+ 
+			AbstractConfigBuilder * builder = ConfigBuilderFactory.get(module_name, mode_name); 
+			builder->parse_partial(mode_config);
+			module_config.add(*builder);
+*/
+			LOG_DEBUG_PRINT("Building mode %s in module %s", 
+											mode_name.c_str(), module_name.c_str());
+
+			if(mode_name == "register_groups") { 
+				RegisterConfigBuilder builder;
+				builder.parse_partial(mode_config);
+				module_config.add(builder);
+			}
+			if(mode_name == "flags") { 
+				FlagConfigBuilder builder;  
+				builder.parse_partial(mode_config);
+				module_config.add(builder);
+			}
+			if(mode_name == "defines") { 
+				DefineConfigBuilder builder; 
+				builder.parse_partial(mode_config);
+				module_config.add(builder);
+			}
+		}
 		m_modules.push_back(module_config);
+		
 		LOG_DEBUG_PRINT("-----------------------------------");
 		LOG_DEBUG_PRINT("Building module %s", module_name.c_str());
 		LOG_DEBUG_PRINT("Module section:\n");
